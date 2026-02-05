df255c565a0ced9443d8a03ae3357af5b8b2bf93
Matthijs Labots <matthijs@caddy.tools>
2026-02-05 15:23:28 +0100

    Revert sonarqube to simple config

--- lua/matthijs990/lazy/sonarqube.lua
+++ lua/matthijs990/lazy/sonarqube.lua
@@ -3,17 +3,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    event = "InsertLeave",
     config = function()
-        -- Filter out sonarqube notifications
-        local original_notify = vim.notify
-        vim.notify = function(msg, level, opts)
-            if type(msg) == "string" and (msg:lower():match("sonar") or msg:lower():match("analyzing")) then
-                return
-            end
-            return original_notify(msg, level, opts)
-        end
-
         require("sonarqube").setup({
             -- Configure your SonarQube server URL and token here
             -- url = "https://your-sonarqube-server.com",
