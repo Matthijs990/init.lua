27565e3ea26c25f193a66f01460e529ca31fe3ee
Matthijs Labots <matthijs@caddy.tools>
2026-02-05 10:30:29 +0100

    Add SonarQube plugin

---
+++ lua/matthijs990/lazy/sonarqube.lua
@@ -0,0 +1,14 @@
+return {
+    "iamkarasik/sonarqube.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require("sonarqube").setup({
+            -- Configure your SonarQube server URL and token here
+            -- url = "https://your-sonarqube-server.com",
+            -- token = "your-sonarqube-token",
+        })
+    end,
+}
+
